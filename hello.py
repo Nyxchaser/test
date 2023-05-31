836e542f4365712b47b96872e3a0cbbac033d5e0
Pixalesce <pixalesce@gmail.com>
2023-05-31 17:19:44 +0800

    something wicked this way comes

--- hello.py
+++ hello.py
@@ -5,3 +5,6 @@ print("Merge conflict resolved!")
 
 print("Another merge conflict")
 print("Merge conflict resolved again!")
+
+mess = "Who would have thought conflicts are so easy to create"
+print(mess)
