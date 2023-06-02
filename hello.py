1bb2b99747ca5b36556c1b715702d15df47dcdd6
Nyx <tessasie15@gmail.com>
2023-06-02 12:49:40 +0800

    Now I've got to clean up your mess again sigh

--- hello.py
+++ hello.py
@@ -8,3 +8,6 @@ print("Merge conflict resolved again!")
 
 mess = "Who would have thought conflicts are so easy to create"
 print(mess)
+
+mess_cleaner = "You think you're funny"
+print(mess_cleaner)
